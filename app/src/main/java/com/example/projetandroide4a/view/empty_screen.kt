57b9940171cf63e75930fe11b7387f854794f954
Damien <damien.nguyen1811@gmail.com>
2025-05-21 17:13:42 +0200

    add basic row for empty screen

---
+++ app/src/main/java/com/example/projetandroide4a/view/empty_screen.kt
@@ -0,0 +1,33 @@
+package com.example.projetandroide4a.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+@Preview
+@Composable
+fun ProductRow(modifier: Modifier = Modifier) {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.LightGray)
+    ) {
+        Text(
+            text = "Pas de série",
+            color = Color.White,
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
